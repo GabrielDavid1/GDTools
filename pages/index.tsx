40f114407d888a93553c03c2710a143c09186019
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-21 16:51:57 -0300

    theme defined

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,23 +9,37 @@ const Home: NextPage = () => {
         <title>GDTools</title>
         <meta name="description" content="my app" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@500&display=swap" rel="stylesheet" />
       </Head>
 
       <div className="functionalities-block">
-        <header className="details">
-          <span className="title-logo">
-            <h2> GDTools </h2>
-          </span>
-          <span className="code-view">
-            <a>
-              <p> View code </p>
-            </a>
-          </span>
-          <p> </p>
-        </header>
         <div className="control-block">
-          <section className="side-area"></section> 
-          <main className="content"> </main>
+
+          <section className="side-area">
+
+          </section> 
+
+          <main className="content">
+            <span className="title-logo">
+              <p> GDTools </p>
+            </span>
+
+            <div className="app-content">
+
+            </div>
+
+            <span className="code-view">
+              <a> 
+                <p> View code </p>
+              </a>
+            </span>
+          </main>
+          
         </div> 
       </div>
       <div className="control-settings"></div>
---
+++ theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+    colors: {
+      BACKGROUND: '#504E4D',
+  
+      PRIMARY_900: '#434141',
+      PRIMARY_800: '#313030',
+      PRIMARY_100: '#504E4D',
+      PRIMARY_50: '#575858',
+  
+      SECONDARY_900: '#FD11F3',
+      SECONDARY_500: '#FF42F7',
+      SECONDARY_400: '#FF7EFA',
+  
+      SHAPE: '#DCDCDC',
+      TITLE: '#FFF',
+  
+      SIDE_GRADIENT: ['#434141', '#393939'],
+    },
+    FONTS: {
+      TITLE: 'Roboto',
+      TEXT: 'Inter_400Regular',
+    },
+  };
