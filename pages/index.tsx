4d473f21becc7c04963aa0c6d4f9c9fc3a207d4d
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-21 15:36:32 -0300

    default struct

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 //import Image from 'next/image'
 
 const Home: NextPage = () => {
@@ -9,15 +9,28 @@ const Home: NextPage = () => {
         <title>GDTools</title>
         <meta name="description" content="my app" />
         <link rel="icon" href="/favicon.ico" />
-      </Head> 
-      <div className="Content"> 
-        <h1>Content</h1>
-      </div>
-      <div className="Settings"> 
-        <h1>Settings</h1>
-      </div>
-    </div>
-  )
-}
+      </Head>
 
-export default Home
+      <div className="functionalities-block">
+        <header className="details">
+          <span className="title-logo">
+            <h2> GDTools </h2>
+          </span>
+          <span className="code-view">
+            <a>
+              <p> View code </p>
+            </a>
+          </span>
+          <p> </p>
+        </header>
+        <div className="control-block">
+          <section className="side-area"></section> 
+          <main className="content"> </main>
+        </div> 
+      </div>
+      <div className="control-settings"></div>
+    </div>
+  );
+};
+
+export default Home;
