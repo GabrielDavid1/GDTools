724442067d23d9320b6fc01ea77064eda9788c9b
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-21 23:15:57 -0300

    fixed problem

--- components/Side/index.tsx
+++ components/Side/index.tsx
@@ -11,37 +11,31 @@ export default function Side (){
    <section className="side-area">
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         Header
       </a>
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         Main 
       </a>
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         View
       </a>
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         Svg
       </a>
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         Image 
       </a>
       <a className="block" 
        href="https://github.com/" 
-       target="_blank"
       >
         Tab Nav
       </a>
