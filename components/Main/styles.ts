8c0641ec09cdc0c7f68126024ece2e03fe189e87
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-23 12:18:27 -0300

    structure mounted

--- components/Main/styles.ts
+++ components/Main/styles.ts
@@ -1,8 +1,25 @@
 import styled from 'styled-components';
 import theme from '../../theme';
 
-export const AppHeader = styled.div``;
+export const AppHeader = styled.div`
+    display: flex;
+    background-color:red;
+    height: 25%;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`;
 
-export const AppBody = styled.div``;
+export const AppBody = styled.div`
+    display: flex;
+    height: 60%;
+    background-color: blue;
+`;
 
-export const AppTab = styled.div``;
+export const AppTab = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 15%;
+    background-color: green;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+`;
