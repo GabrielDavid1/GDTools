19bc64fdf53e20224217b5233805887b4ba62648
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-23 21:55:06 -0300

    Box Shadow section in settngs has finished

--- components/Settings/ModelSetting/Div/BoxShadow.tsx
+++ components/Settings/ModelSetting/Div/BoxShadow.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 export default function BoxShadow () {
  const [value, setValue] = useState<number>(0);
+ 
  return (
    <div className="boxShadow-area">
       <h2 onClick={() => console.log(value)}> Box Shadow </h2> 
