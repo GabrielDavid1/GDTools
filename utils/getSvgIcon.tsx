33c4b3dcf0b7e46d612d6cc2825f74e0768f1864
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-25 17:43:20 -0300

    create getSvgIcon function

---
+++ utils/getSvgIcon.tsx
@@ -0,0 +1,21 @@
+export function getSvgIcon(type: string) {
+  switch (type) {
+    case "circle":
+      return (
+        <div className="field-tab">
+          <svg
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="20%"
+            viewBox="0 0 120 120"
+            enable-background="new 0 0 120 120"
+          >
+            <circle cx="60" cy="60.834" r="54.167" />
+          </svg>
+        </div>
+      );
+  }
+}
