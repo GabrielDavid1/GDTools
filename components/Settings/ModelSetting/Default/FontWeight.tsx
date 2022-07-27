0e9f7579386399dccaa17106609b01c5e440ab78
gabrieldavid <gabrieldavid75315@gmail.com>
2022-07-26 21:18:17 -0300

    Font Weight to element has finished in settings

--- components/Settings/ModelSetting/Default/FontWeight.tsx
+++ components/Settings/ModelSetting/Default/FontWeight.tsx
@@ -1,13 +1,24 @@
 //React
-import React, { useState } from 'react';
+import React from 'react';
+
+//Contexts
+import { useFuncs } from '../../../../contexts/Functionalities';
 
 export default function FontWeight () {
- const [value, setValue] = useState<number>(0);
- 
+ const { funcs, setFuncs, selected } = useFuncs();
+
+ function handleChange (e = {} as React.ChangeEvent<HTMLSelectElement>) {
+     if (selected.config !== undefined) {
+         selected.config.fontWeight = e.target.value;
+     }
+     setFuncs([...funcs]);
+     e.preventDefault();
+ }
+
  return (
    <div className="fontWeight-area">
       <h2> Font Weight </h2> 
-      <select onChange={(e) => setValue(Number(e.target.value))}>
+      <select onChange={(e) => handleChange(e)}>
         <option value={0}>none</option>
         <option value={100}>100</option>
         <option value={200}>200</option>
@@ -17,7 +28,7 @@ export default function FontWeight () {
         <option value={600}>600</option>
         <option value={700}>700</option>
         <option value={800}>800</option>
-        <option value={900}>900</option>
+        <option value={900}>Bold</option>
       </select>
    </div>
  )
