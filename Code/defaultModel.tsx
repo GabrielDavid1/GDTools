9cfae256124e8816a97b2764962fef7d0a6970bf
gabrieldavid <gabrieldavid75315@gmail.com>
2022-08-01 18:10:28 -0300

    fix ; bug

--- Code/defaultModel.tsx
+++ Code/defaultModel.tsx
@@ -4,7 +4,7 @@ export default function defaultModel(type:string) {
   const styles = "import styled from 'styled-components/native';"+'\n\n'
                  +"import { TouchableOpacity } from 'react-native';"+'\n\n'
                  +"import { Entypo, Ionicons, Foundation, AntDesign, Foundation, Fontisto, Octicons, FontAwesome5, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';"+'\n\n'
-                 +"export const Container = styled.View`flex: 1;`;\n export const Header = styled.View`\nwidth:100%\nheight:25%\nbackground-color:'#f8f8f8'\nflex-direction:row\njustify-content:center\nalign-items:center\ngap:0px\n`\nexport const Main= styled.View`background-color:'#dfdfdf'\nwidth:100%\nheight:100%\nflex-direction:row\njustify-content:center\nalign-items:center\ngap:0px\n`\nexport const TabNav= styled.View`background-color:'#c4c4c4'\nwidth:100%\nheight:15%\nflex-direction:row\njustify-content:center\nalign-items:center\ngap:0px\n`;\n";
+                 +"export const Container = styled.View`flex: 1;`;\n export const Header = styled.View`\nwidth:100%;\nheight:25%;\nbackground-color:'#f8f8f8';\nflex-direction:row;\njustify-content:center;\nalign-items:center;\ngap:0px;\n`;\nexport const Main = styled.View`background-color:'#dfdfdf';\nwidth:100%;\nheight:100%;\nflex-direction:row;\njustify-content:center;\nalign-items:center;\ngap:0px;\n`;\nexport const TabNav = styled.View`background-color:'#c4c4c4';\nwidth:100%;\nheight:15%;\nflex-direction:row;\njustify-content:center;\nalign-items:center;\ngap:0px;\n`;\n";
 
   const variables = "";
 
